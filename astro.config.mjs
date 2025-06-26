97ff30e152b6ffecc9413ef52879decc36a42b86
mauro1972 <yacu72@gmail.com>
2025-06-26 20:15:04 -0300

    changing site url in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,10 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import icon from "astro-icon";
 
-const LIVE_URL = "https://mauro-astro.github.io";
-
-if (isBuild) {
-  BASE_URL = LIVE_URL;
-}
 
 // https://astro.build/config
 export default defineConfig({
-  site: BASE_URL,
+  site: 'https://mauro-astro.github.io',
   base: 'mauro1972',
   integrations: [react(), icon()],
 });
