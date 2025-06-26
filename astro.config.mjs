f442a38a48a0d2a1e1079663bd781cc95953d85f
mauro1972 <yacu72@gmail.com>
2025-06-26 19:37:48 -0300

    adding action

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,15 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import icon from "astro-icon";
 
+const LIVE_URL = "https://mauro-astro.github.io";
+
+if (isBuild) {
+  BASE_URL = LIVE_URL;
+}
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-  integrations: [icon()]
+  site: BASE_URL,
+  base: 'mauro1972',
+  integrations: [react(), icon()],
 });
