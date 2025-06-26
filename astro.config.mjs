5359bb8e0b6b834c82fd7d4f28dbb422390a45bd
mauro1972 <yacu72@gmail.com>
2025-06-26 20:44:12 -0300

    fixing url path

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mauro-astro.github.io',
+  site: 'https://mauro1972.github.io',
+  base: '/mauro-astro',
   integrations: [react(), icon()],
 });
